b4e5e1df2a6e9a9e8b255ed0e0d79214b0eb3dd7
rusty1s <matthias.fey@tu-dortmund.de>
2017-12-16 18:17:49 +0100

    removed tests on cffi

--- torch_scatter/src/cpu.c
+++ torch_scatter/src/cpu.c
@@ -1,9 +1,8 @@
 #include <TH/TH.h>
 
 #define scatter_(NAME) TH_CONCAT_4(scatter_, NAME, _, Real)
-#define check_(NAME) TH_CONCAT_4(check_, NAME, _, Real)
 
-inline void check_inBoundaries(int idx, int size, long *free) {
+inline void assertInBoundaries(int idx, int size, long *free) {
   if (idx < 0 || idx >= size) { THFree(free); THError("Invalid index"); }
 }
 
--- torch_scatter/src/generic/cpu.c
+++ torch_scatter/src/generic/cpu.c
@@ -2,29 +2,12 @@
 #define TH_GENERIC_FILE "generic/cpu.c"
 #else
 
-inline void check_(asserts)(THTensor *output, THLongTensor *index, THTensor *input, int dim) {
-  // Assert same dimensionality.
-  THArgCheck(dim >= 0 && dim < THTensor_(nDimension)(output), 4, "Index dimension is out of bounds");
-  THArgCheck(THLongTensor_nDimension(index) == THTensor_(nDimension)(input), 2, "Index tensor must have same dimensions as input tensor");
-  THArgCheck(THTensor_(nDimension)(input) == THTensor_(nDimension)(output), 3, "Input tensor must have same dimensions as output tensor");
-
-  // Assert same tensor sizes across index and input.
-  THLongStorage *indexDims = THLongTensor_newSizeOf(index);
-  THArgCheck(THTensor_(isSize)(input, indexDims), 2, "Index tensor must have the same size as input tensor.");
-  THLongStorage_free(indexDims);
-
-  // Assert same tensor sizes across input and output apart from specified dimension.
-  for (int d = 0; d < THTensor_(nDimension)(output); d++) {
-    if (d != dim) THArgCheck(THTensor_(size)(output, d) == THTensor_(size)(input, d), 3, "Input tensor must have same size as output tensor apart from the specified dimension");
-  }
-}
-
 void scatter_(add)(THTensor *output, THLongTensor *index, THTensor *input, int dim) {
-  check_(asserts)(output, index, input, dim); long idx;
+  long idx;
   TH_TENSOR_DIM_APPLY3(real, output, real, input, long, index, dim,
     for (int i = 0; i < THLongTensor_size(index, dim); i++) {
       idx = *(index_data + i * index_stride);
-      check_inBoundaries(idx, output_size, TH_TENSOR_DIM_APPLY_counter);
+      assertInBoundaries(idx, output_size, TH_TENSOR_DIM_APPLY_counter);
       output_data[idx] += *(input_data + i * input_stride);
     })
 }
