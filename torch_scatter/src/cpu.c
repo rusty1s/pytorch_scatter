bc666bf5fc87bfc247f70cd374bee4b4b55754f0
rusty1s <matthias.fey@tu-dortmund.de>
2017-12-16 19:00:41 +0100

    update for pytorch 0.3

--- torch_scatter/src/cpu.c
+++ torch_scatter/src/cpu.c
@@ -1,8 +1,10 @@
 #include <TH/TH.h>
 
+#include "THTensorDimApply.h"
+
 #define scatter_(NAME) TH_CONCAT_4(scatter_, NAME, _, Real)
 
-inline void assertInBoundaries(int idx, int size, long *free) {
+inline void assertIndexInBoundaries(int idx, int size, long *free) {
   if (idx < 0 || idx >= size) { THFree(free); THError("Invalid index"); }
 }
 
--- torch_scatter/src/generic/cpu.c
+++ torch_scatter/src/generic/cpu.c
@@ -4,10 +4,11 @@
 
 void scatter_(add)(THTensor *output, THLongTensor *index, THTensor *input, int dim) {
   long idx;
-  TH_TENSOR_DIM_APPLY3(real, output, real, input, long, index, dim,
+  TH_TENSOR_DIM_APPLY3(real, output, real, input, int64_t, index, dim,
+    TH_TENSOR_DIM_APPLY3_SIZE_EQ_EXCEPT_DIM,
     for (int i = 0; i < THLongTensor_size(index, dim); i++) {
       idx = *(index_data + i * index_stride);
-      assertInBoundaries(idx, output_size, TH_TENSOR_DIM_APPLY_counter);
+      assertIndexInBoundaries(idx, output_size, TH_TENSOR_DIM_APPLY_counter);
       output_data[idx] += *(input_data + i * input_stride);
     })
 }
