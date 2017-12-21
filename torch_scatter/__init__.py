3f1346dc8867c143fdf2146d48860ce75ddbcd5d
rusty1s <matthias.fey@tu-dortmund.de>
2017-12-21 13:53:58 +0100

    cleaner

--- torch_scatter/__init__.py
+++ torch_scatter/__init__.py
@@ -1,2 +1,16 @@
-from .version import __version__  # noqa
-from .functions import *  # noqa
+from functions.add import scatter_add_, scatter_add
+from functions.sub import scatter_sub_, scatter_sub
+from functions.mul import scatter_mul_, scatter_mul
+from functions.div import scatter_div_, scatter_div
+from functions.mean import scatter_mean_, scatter_mean
+from functions.max import scatter_max_, scatter_max
+from functions.min import scatter_min_, scatter_min
+
+from .version import __version__
+
+__all__ = [
+    'scatter_add_', 'scatter_add', 'scatter_sub_', 'scatter_sub',
+    'scatter_mul_', 'scatter_mul', 'scatter_div_', 'scatter_div',
+    'scatter_mean_', 'scatter_mean', 'scatter_max_', 'scatter_max',
+    'scatter_min_', 'scatter_min', '__version__'
+]
