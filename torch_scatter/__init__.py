601e686e6e2167eb3202c10c224444bc6bacbbf8
rusty1s <matthias.fey@tu-dortmund.de>
2017-12-21 20:38:46 +0100

    bugfix

--- torch_scatter/__init__.py
+++ torch_scatter/__init__.py
@@ -1,10 +1,10 @@
-from functions.add import scatter_add_, scatter_add
-from functions.sub import scatter_sub_, scatter_sub
-from functions.mul import scatter_mul_, scatter_mul
-from functions.div import scatter_div_, scatter_div
-from functions.mean import scatter_mean_, scatter_mean
-from functions.max import scatter_max_, scatter_max
-from functions.min import scatter_min_, scatter_min
+from .functions.add import scatter_add_, scatter_add
+from .functions.sub import scatter_sub_, scatter_sub
+from .functions.mul import scatter_mul_, scatter_mul
+from .functions.div import scatter_div_, scatter_div
+from .functions.mean import scatter_mean_, scatter_mean
+from .functions.max import scatter_max_, scatter_max
+from .functions.min import scatter_min_, scatter_min
 
 from .version import __version__
 
