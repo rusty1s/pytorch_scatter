cf15a2dd582d4d3543697f09bec86d76777e3511
rusty1s <matthias.fey@tu-dortmund.de>
2017-12-24 13:24:53 +0100

    c99 c

--- torch_scatter/src/generic/cpu.c
+++ torch_scatter/src/generic/cpu.c
@@ -3,24 +3,27 @@
 #else
 
 void scatter_(mul)(int dim, THTensor *output, THLongTensor *index, THTensor *input) {
+  int64_t i;
   TH_TENSOR_DIM_APPLY3(real, output, int64_t, index, real, input, dim, TH_TENSOR_DIM_APPLY3_SIZE_EQ_EXCEPT_DIM,
-    for (int64_t i = 0; i < THLongTensor_size(index, dim); i++) {
+    for (i = 0; i < THLongTensor_size(index, dim); i++) {
       assertIndexInBoundaries(index_data[i], output_size, TH_TENSOR_DIM_APPLY_counter);
       output_data[index_data[i]] *= input_data[i];
     })
 }
 
 void scatter_(div)(int dim, THTensor *output, THLongTensor *index, THTensor *input) {
+  int64_t i;
   TH_TENSOR_DIM_APPLY3(real, output, int64_t, index, real, input, dim, TH_TENSOR_DIM_APPLY3_SIZE_EQ_EXCEPT_DIM,
-    for (int64_t i = 0; i < THLongTensor_size(index, dim); i++) {
+    for (i = 0; i < THLongTensor_size(index, dim); i++) {
       assertIndexInBoundaries(index_data[i], output_size, TH_TENSOR_DIM_APPLY_counter);
       output_data[index_data[i]] /= input_data[i];
     })
 }
 
 void scatter_(mean)(int dim, THTensor *output, THLongTensor *index, THTensor *input, THTensor *count) {
+  int64_t i;
   TH_TENSOR_DIM_APPLY4(real, output, int64_t, index, real, input, real, count, dim,
-    for (int64_t i = 0; i < THLongTensor_size(index, dim); i++) {
+    for (i = 0; i < THLongTensor_size(index, dim); i++) {
       assertIndexInBoundaries(index_data[i], output_size, TH_TENSOR_DIM_APPLY_counter);
       output_data[index_data[i]] += input_data[i];
       count_data[index_data[i]]++;
@@ -28,8 +31,9 @@ void scatter_(mean)(int dim, THTensor *output, THLongTensor *index, THTensor *in
 }
 
 void scatter_(max)(int dim, THTensor *output, THLongTensor *index, THTensor *input, THLongTensor *arg) {
+  int64_t i;
   TH_TENSOR_DIM_APPLY4(real, output, int64_t, index, real, input, int64_t, arg, dim,
-    for (int64_t i = 0; i < THLongTensor_size(index, dim); i++) {
+    for (i = 0; i < THLongTensor_size(index, dim); i++) {
       assertIndexInBoundaries(index_data[i], output_size, TH_TENSOR_DIM_APPLY_counter);
       if (input_data[i] >= output_data[index_data[i]]) {
         output_data[index_data[i]] = input_data[i];
@@ -39,8 +43,9 @@ void scatter_(max)(int dim, THTensor *output, THLongTensor *index, THTensor *inp
 }
 
 void scatter_(min)(int dim, THTensor *output, THLongTensor *index, THTensor *input, THLongTensor *arg) {
+  int64_t i;
   TH_TENSOR_DIM_APPLY4(real, output, int64_t, index, real, input, int64_t, arg, dim,
-    for (int64_t i = 0; i < THLongTensor_size(index, dim); i++) {
+    for (i = 0; i < THLongTensor_size(index, dim); i++) {
       assertIndexInBoundaries(index_data[i], output_size, TH_TENSOR_DIM_APPLY_counter);
       if (input_data[i] <= output_data[index_data[i]]) {
         output_data[index_data[i]] = input_data[i];
@@ -50,8 +55,9 @@ void scatter_(min)(int dim, THTensor *output, THLongTensor *index, THTensor *inp
 }
 
 void index_backward(int dim, THTensor *output, THLongTensor *index, THTensor *grad, THLongTensor *arg) {
+  int64_t i;
   TH_TENSOR_DIM_APPLY4(real, output, int64_t, index, real, grad, int64_t, arg, dim,
-    for (int64_t i = 0; i < THLongTensor_size(index, dim); i++) {
+    for (i = 0; i < THLongTensor_size(index, dim); i++) {
       if (arg_data[index_data[i]] == i) output_data[i] = grad_data[index_data[i]];
     })
 }
