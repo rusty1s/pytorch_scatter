4d86488ed8a5dcc810c2d62ac393662893658283
rusty1s <matthias.fey@tu-dortmund.de>
2017-12-27 18:32:13 +0100

    typo

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ docs_require = ['Sphinx', 'sphinx_rtd_theme']
 setup(
     name='torch_scatter',
     version=__version__,
-    description='PyTorch extension for various scatter methods',
+    description='PyTorch extension for various scatter-operations',
     url='https://github.com/rusty1s/pytorch_scatter',
     author='Matthias Fey',
     author_email='matthias.fey@tu-dortmund.de',
