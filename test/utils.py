d81ed49dc7211860423c2be4fc4e3192e466ed7f
rusty1s <matthias.fey@tu-dortmund.de>
2017-12-28 21:02:50 +0100

    bugfixes

--- test/utils.py
+++ test/utils.py
@@ -2,6 +2,7 @@ import torch
 from torch._tensor_docs import tensor_classes
 
 tensors = [t[:-4] for t in tensor_classes]
+tensors.remove('ShortTensor')  # TODO: PyTorch `atomicAdd` bug with short type.
 
 
 def Tensor(str, x):
