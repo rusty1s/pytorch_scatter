782fb51c49c9ac5dd4706c96add8cc04c429b58d
rusty1s <matthias.fey@tu-dortmund.de>
2017-12-28 22:05:29 +0100

    include file fix

--- torch_scatter/__init__.py
+++ torch_scatter/__init__.py
@@ -6,7 +6,7 @@ from .functions.mean import scatter_mean_, scatter_mean
 from .functions.max import scatter_max_, scatter_max
 from .functions.min import scatter_min_, scatter_min
 
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 
 __all__ = [
     'scatter_add_', 'scatter_add', 'scatter_sub_', 'scatter_sub',
