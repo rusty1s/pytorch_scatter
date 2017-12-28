1f019c20fcae70b3c0e2eac9964d8e23201d4ea6
rusty1s <matthias.fey@tu-dortmund.de>
2017-12-28 10:37:02 +0100

    fill value added

--- test/test_forward.py
+++ test/test_forward.py
@@ -16,18 +16,19 @@ f.close()
 @pytest.mark.parametrize('tensor,i', product(tensors, range(len(data))))
 def test_forward_cpu(tensor, i):
     name = data[i]['name']
-    output = Tensor(tensor, data[i]['output'])
     index = torch.LongTensor(data[i]['index'])
     input = Tensor(tensor, data[i]['input'])
     dim = data[i]['dim']
-    expected = Tensor(tensor, data[i]['expected'])
+    fill_value = data[i]['fill_value']
+    expected = torch.FloatTensor(data[i]['expected']).type_as(input)
+    output = expected.new(expected.size()).fill_(fill_value)
 
     func = getattr(torch_scatter, 'scatter_{}_'.format(name))
     func(output, index, input, dim)
     assert output.tolist() == expected.tolist()
 
     func = getattr(torch_scatter, 'scatter_{}'.format(name))
-    output = func(index, input, dim)
+    output = func(index, input, dim, fill_value=fill_value)
     assert output.tolist() == expected.tolist()
 
 
@@ -35,16 +36,17 @@ def test_forward_cpu(tensor, i):
 @pytest.mark.parametrize('tensor,i', product(tensors, range(len(data))))
 def test_forward_gpu(tensor, i):
     name = data[i]['name']
-    output = Tensor(tensor, data[i]['output']).cuda()
     index = torch.LongTensor(data[i]['index']).cuda()
     input = Tensor(tensor, data[i]['input']).cuda()
     dim = data[i]['dim']
-    expected = Tensor(tensor, data[i]['expected'])
+    fill_value = data[i]['fill_value']
+    expected = torch.FloatTensor(data[i]['expected']).type_as(input)
+    output = expected.new(expected.size()).fill_(fill_value).cuda()
 
     func = getattr(torch_scatter, 'scatter_{}_'.format(name))
     func(output, index, input, dim)
     assert output.cpu().tolist() == expected.tolist()
 
     func = getattr(torch_scatter, 'scatter_{}'.format(name))
-    output = func(index, input, dim)
+    output = func(index, input, dim, fill_value=fill_value)
     assert output.cpu().tolist() == expected.tolist()
