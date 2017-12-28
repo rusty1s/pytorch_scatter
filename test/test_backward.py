0a5eeeadba2d8089d6a3809aaad36837d095c306
rusty1s <matthias.fey@tu-dortmund.de>
2017-12-28 21:21:12 +0100

    first cuda, then variable call

--- test/test_backward.py
+++ test/test_backward.py
@@ -35,12 +35,12 @@ def test_backward_cpu(tensor, i):
 @pytest.mark.parametrize('tensor,i', product(tensors, range(len(data))))
 def test_backward_gpu(tensor, i):  # pragma: no cover
     name = data[i]['name']
-    index = V(torch.LongTensor(data[i]['index'])).cuda()
-    input = V(Tensor(tensor, data[i]['input']), requires_grad=True).cuda()
+    index = V(torch.LongTensor(data[i]['index']).cuda())
+    input = V(Tensor(tensor, data[i]['input']).cuda(), requires_grad=True)
     dim = data[i]['dim']
     fill_value = data[i]['fill_value']
     grad = Tensor(tensor, data[i]['grad']).cuda()
-    output = V(grad.new(grad.size()).fill_(fill_value)).cuda()
+    output = V(grad.new(grad.size()).fill_(fill_value).cuda())
     expected = Tensor(tensor, data[i]['expected'])
 
     func = getattr(torch_scatter, 'scatter_{}_'.format(name))
