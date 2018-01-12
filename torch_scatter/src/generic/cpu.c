a7beacabbd5cc07121b31a1b5bd43a8e4a241df4
rusty1s <matthias.fey@tu-dortmund.de>
2018-01-12 17:39:26 +0100

    bugfix with tensor strides

--- torch_scatter/src/generic/cpu.c
+++ torch_scatter/src/generic/cpu.c
@@ -3,62 +3,67 @@
 #else
 
 void scatter_(mul)(int dim, THTensor *output, THLongTensor *index, THTensor *input) {
-  int64_t i;
+  int64_t i, idx;
   TH_TENSOR_DIM_APPLY3(real, output, int64_t, index, real, input, dim, TH_TENSOR_DIM_APPLY3_SIZE_EQ_EXCEPT_DIM,
     for (i = 0; i < THLongTensor_size(index, dim); i++) {
-      assertIndexInBoundaries(index_data[i], output_size, TH_TENSOR_DIM_APPLY_counter);
-      output_data[index_data[i]] *= input_data[i];
+      idx = *(index_data + i * index_stride);
+      assertIndexInBoundaries(idx, output_size, TH_TENSOR_DIM_APPLY_counter);
+      output_data[idx * output_stride] *= *(input_data + i * input_stride);
     })
 }
 
 void scatter_(div)(int dim, THTensor *output, THLongTensor *index, THTensor *input) {
-  int64_t i;
+  int64_t i, idx;
   TH_TENSOR_DIM_APPLY3(real, output, int64_t, index, real, input, dim, TH_TENSOR_DIM_APPLY3_SIZE_EQ_EXCEPT_DIM,
     for (i = 0; i < THLongTensor_size(index, dim); i++) {
-      assertIndexInBoundaries(index_data[i], output_size, TH_TENSOR_DIM_APPLY_counter);
-      output_data[index_data[i]] /= input_data[i];
+      idx = *(index_data + i * index_stride);
+      assertIndexInBoundaries(idx, output_size, TH_TENSOR_DIM_APPLY_counter);
+      output_data[idx * output_stride] /= *(input_data + i * input_stride);
     })
 }
 
 void scatter_(mean)(int dim, THTensor *output, THLongTensor *index, THTensor *input, THTensor *count) {
-  int64_t i;
+  int64_t i, idx;
   TH_TENSOR_DIM_APPLY4(real, output, int64_t, index, real, input, real, count, dim,
     for (i = 0; i < THLongTensor_size(index, dim); i++) {
       assertIndexInBoundaries(index_data[i], output_size, TH_TENSOR_DIM_APPLY_counter);
-      output_data[index_data[i]] += input_data[i];
-      count_data[index_data[i]]++;
+      output_data[idx * output_stride] += *(input_data + i * input_stride);
+      output_data[idx * count_stride]++;
     })
 }
 
 void scatter_(max)(int dim, THTensor *output, THLongTensor *index, THTensor *input, THLongTensor *arg) {
-  int64_t i;
+  int64_t i, idx;
   TH_TENSOR_DIM_APPLY4(real, output, int64_t, index, real, input, int64_t, arg, dim,
     for (i = 0; i < THLongTensor_size(index, dim); i++) {
-      assertIndexInBoundaries(index_data[i], output_size, TH_TENSOR_DIM_APPLY_counter);
-      if (input_data[i] >= output_data[index_data[i]]) {
-        output_data[index_data[i]] = input_data[i];
-        arg_data[index_data[i]] = i;
+      idx = *(index_data + i * index_stride);
+      assertIndexInBoundaries(idx, output_size, TH_TENSOR_DIM_APPLY_counter);
+      if (*(input_data + i * input_stride) >= *(output_data + idx * output_stride)) {
+        output_data[idx * output_stride] = *(input_data + i * input_stride);
+        arg_data[idx * arg_stride] = i;
       }
     })
 }
 
 void scatter_(min)(int dim, THTensor *output, THLongTensor *index, THTensor *input, THLongTensor *arg) {
-  int64_t i;
+  int64_t i, idx;
   TH_TENSOR_DIM_APPLY4(real, output, int64_t, index, real, input, int64_t, arg, dim,
     for (i = 0; i < THLongTensor_size(index, dim); i++) {
-      assertIndexInBoundaries(index_data[i], output_size, TH_TENSOR_DIM_APPLY_counter);
-      if (input_data[i] <= output_data[index_data[i]]) {
-        output_data[index_data[i]] = input_data[i];
-        arg_data[index_data[i]] = i;
+      idx = *(index_data + i * index_stride);
+      assertIndexInBoundaries(idx, output_size, TH_TENSOR_DIM_APPLY_counter);
+      if (*(input_data + i * input_stride) <= *(output_data + idx * output_stride)) {
+        output_data[idx * output_stride] = *(input_data + i * input_stride);
+        arg_data[idx * arg_stride] = i;
       }
     })
 }
 
 void index_backward(int dim, THTensor *output, THLongTensor *index, THTensor *grad, THLongTensor *arg) {
-  int64_t i;
+  int64_t i, idx;
   TH_TENSOR_DIM_APPLY4(real, output, int64_t, index, real, grad, int64_t, arg, dim,
     for (i = 0; i < THLongTensor_size(index, dim); i++) {
-      if (arg_data[index_data[i]] == i) output_data[i] = grad_data[index_data[i]];
+      idx = *(index_data + i * index_stride);
+      if (*(arg_data + idx * arg_stride) == i) output_data[i * output_stride] = *(grad_data + idx * grad_stride);
     })
 }
 
