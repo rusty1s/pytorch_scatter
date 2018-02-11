correct gradient computations

torch_scatter/functions/scatter.py
@@ -1,58 +1,17 @@
-from itertools import chain
-
 import torch
 from torch.autograd import Function
 
-from .._ext import ffi
+from .ffi import scatter as ffi_scatter
 
 
-def has_arg(name):
-    return name in ['max', 'min']
-
-
-def _scatter(name, dim, *data):
-    a, b, c = data[:3]
-
-    # Assert index dimension is valid.
-    assert dim >= 0 and dim < a.dim(), 'Index dimension is out of bounds'
-
-    # Assert same dimensionality across all inputs.
-    assert b.dim() == c.dim(), ('Index tensor must have same dimensions as '
-                                'input tensor')
-    assert a.dim() == c.dim(), ('Input tensor must have same dimensions as '
-                                'output tensor')
-
-    # Assert same tensor length across index and input.
-    assert b.numel() == c.numel(), ('Index tensor must have same size as '
-                                    'input tensor')
-
-    # Assert same tensor sizes across input and output apart from `dim`.
-    for d in chain(range(dim), range(dim + 1, a.dim())):
-        assert a.size(d) == c.size(d), (
-            'Input tensor must have same size as output tensor apart from the '
-            'specified dimension')
-
-    typename = type(data[0]).__name__.replace('Tensor', '')
-    cuda = 'cuda_' if data[0].is_cuda else ''
-    func = getattr(ffi, 'scatter_{}_{}{}'.format(name, cuda, typename))
-    func(dim, *data)
-    return (data[0], data[3]) if has_arg(name) else data[0]
-
-
-def index_backward(dim, index, grad, arg):  # pragma: no cover
-    typename = type(grad).__name__.replace('Tensor', '')
-    cuda = 'cuda_' if grad.is_cuda else ''
-    func = getattr(ffi, 'index_backward_{}{}'.format(cuda, typename))
-    output = grad.new(index.size()).fill_(0)
-    func(dim, output, index, grad, arg)
-    return output
-
-
-class _Scatter(Function):
+class Scatter(Function):
     def __init__(self, name, dim):
-        super(_Scatter, self).__init__()
+        super(Scatter, self).__init__()
         self.name = name
         self.dim = dim
+
+    def save_for_backward_step(self, *data):
+        raise NotImplementedError
 
     def forward(self, *data):
         assert not self.needs_input_grad[1], 'Can\'t differentiate the index'
@@ -60,17 +19,10 @@ class _Scatter(Function):
         self.mark_dirty(data[0])  # Mark output as dirty.
         self.len = len(data)  # Save number of arguments for backward step.
 
-        _scatter(self.name, self.dim, *data)
+        output = ffi_scatter(self.name, self.dim, *data)
+        self.save_for_backward_step(*data)
 
-        # `scatter_min` and `scatter_max` additionally return the `argmax`
-        # respectively `argmin`. Therefore, we need to save the `arg` for the
-        # backward pass.
-        if has_arg(self.name):
-            self.save_for_backward(data[1], data[3])
-            return data[0], data[3]
-        else:
-            self.save_for_backward(data[1])
-            return data[0]
+        return output
 
     def backward(self, *data):  # pragma: no cover
         grad_output = grad_input = None
@@ -78,24 +30,53 @@ class _Scatter(Function):
         if self.needs_input_grad[0]:
             grad_output = data[0]
 
-        # Different grad computation of `input` if `scatter_max` or
-        # `scatter_min` was used.
-        if self.needs_input_grad[2] and not has_arg(self.name):
-            index, = self.saved_variables
-            grad_input = data[0].gather(self.dim, index.data)
+        # Call grad computation of `input` for the specific scatter operation.
+        if self.needs_input_grad[2]:
+            grad_input = self.backward_step(data[0], *self.saved_variables)
 
-        if self.needs_input_grad[2] and has_arg(self.name):
-            index, arg = self.saved_variables
-            data = (index.data, data[0], arg.data)
-            grad_input = index_backward(self.dim, *data)
-
-        # Return and fill with empty grads for non-differentiable passed
-        # arguments in forward pass.
+        # Return and fill with empty grads for non-differentiable arguments.
         return (grad_output, None, grad_input) + (None, ) * (self.len - 3)
 
+    def backward_step(self, *data):
+        raise NotImplementedError
 
-def scatter(name, dim, *data):
+
+def scatter(Clx, name, dim, *data):
     if torch.is_tensor(data[0]):
-        return _scatter(name, dim, *data)
+        return ffi_scatter(name, dim, *data)
     else:
-        return _Scatter(name, dim)(*data)
+        return Clx(dim)(*data)
+
+
+# def index_backward(dim, index, grad, arg):  # pragma: no cover
+#     typename = type(grad).__name__.replace('Tensor', '')
+#     cuda = 'cuda_' if grad.is_cuda else ''
+#     func = getattr(ffi, 'index_backward_{}{}'.format(cuda, typename))
+#     output = grad.new(index.size()).fill_(0)
+#     func(dim, output, index, grad, arg)
+#     return output
+
+# def _scatter_backward(name, dim, saved, *data):
+#     # saved = (index, ), (index, arg) or (index, count)
+
+#     print(name)
+#     print(len(data))
+#     print(len(saved))
+#     print(saved[1].size())
+#     # data = (grad, )
+#     # index, = seved
+#     if has_arg(name):
+#         return index_backward(dim, saved[0].data, data[0], saved[1].data)
+
+#     if has_count(name):
+#         return (data[0] / saved[1]).gather(dim, saved[0].data)
+#     # Different grad computation of `input` if `scatter_max` or
+#     # `scatter_min` was used.
+#     # if self.needs_input_grad[2] and not has_arg(self.name):
+#     #     index, = self.saved_variables
+#     #     grad_input = data[0].gather(self.dim, index.data)
+
+#     # if self.needs_input_grad[2] and has_arg(self.name):
+#     #     index, arg = self.saved_variables
+#     #     data = (index.data, data[0], arg.data)
+#     grad_input = index_backward(self.dim, *data)