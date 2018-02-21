a2c74afd5a794845f27d69faf0feac56f7cac109
rusty1s <matthias.fey@tu-dortmund.de>
2018-02-21 14:11:15 +0100

    allow nvcc sudo

--- build.sh
+++ build.sh
@@ -7,4 +7,4 @@ SRC_DIR=torch_scatter/kernel
 BUILD_DIR=torch_scatter/build
 
 mkdir -p $BUILD_DIR
-nvcc -c -o $BUILD_DIR/kernel.so $SRC_DIR/kernel.cu -arch=sm_30 -Xcompiler -fPIC -shared -I$TORCH/lib/include/TH -I$TORCH/lib/include/THC -I$SRC_DIR
+$(which nvcc) -c -o $BUILD_DIR/kernel.so $SRC_DIR/kernel.cu -arch=sm_30 -Xcompiler -fPIC -shared -I$TORCH/lib/include/TH -I$TORCH/lib/include/THC -I$SRC_DIR
