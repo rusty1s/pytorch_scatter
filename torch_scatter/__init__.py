d10e14265b46bc9d81bf458d2096f79c8816f00a
rusty1s <matthias.fey@tu-dortmund.de>
2018-04-28 10:31:17 +0200

    scatter mul

--- torch_scatter/__init__.py
+++ torch_scatter/__init__.py
@@ -1,7 +1,10 @@
 from .add import scatter_add
 from .sub import scatter_sub
+from .mul import scatter_mul
 from .mean import scatter_mean
 
 __version__ = '1.0.0'
 
-__all__ = ['scatter_add', 'scatter_sub', 'scatter_mean', '__version__']
+__all__ = [
+    'scatter_add', 'scatter_sub', 'scatter_mul', 'scatter_mean', '__version__'
+]
