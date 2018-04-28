8b8df250b315afba0fe68c2a78f1782df90ef160
rusty1s <matthias.fey@tu-dortmund.de>
2018-04-28 08:10:56 +0200

    new pytorch 0.4.0 format

--- test/test_forward.py
+++ test/test_forward.py
@@ -1,67 +1,49 @@
-from os import path as osp
 from itertools import product
 
 import pytest
-import json
 import torch
 import torch_scatter
 
-from .utils import tensors, Tensor
+from .utils import dtypes, devices, tensor
 
-f = open(osp.join(osp.dirname(__file__), 'forward.json'), 'r')
-data = json.load(f)
-f.close()
-
-
-@pytest.mark.parametrize('tensor,i', product(tensors, range(len(data))))
-def test_forward_cpu(tensor, i):
-    name = data[i]['name']
-    index = torch.LongTensor(data[i]['index'])
-    input = Tensor(tensor, data[i]['input'])
-    dim = data[i]['dim']
-    fill_value = data[i]['fill_value']
-    expected = torch.FloatTensor(data[i]['expected']).type_as(input)
-    output = expected.new(expected.size()).fill_(fill_value)
-
-    func = getattr(torch_scatter, 'scatter_{}_'.format(name))
-    result = func(output, index, input, dim)
-    assert output.tolist() == expected.tolist()
-    if 'expected_arg' in data[i]:
-        expected_arg = torch.LongTensor(data[i]['expected_arg'])
-        assert result[1].tolist() == expected_arg.tolist()
-
-    func = getattr(torch_scatter, 'scatter_{}'.format(name))
-    result = func(index, input, dim, fill_value=fill_value)
-    if 'expected_arg' not in data[i]:
-        assert result.tolist() == expected.tolist()
-    else:
-        expected_arg = torch.LongTensor(data[i]['expected_arg'])
-        assert result[0].tolist() == expected.tolist()
-        assert result[1].tolist() == expected_arg.tolist()
+tests = [{
+    'name': 'add',
+    'src': [[2, 0, 1, 4, 3], [0, 2, 1, 3, 4]],
+    'index': [[4, 5, 4, 2, 3], [0, 0, 2, 2, 1]],
+    'fill_value': 0,
+    'expected': [[0, 0, 4, 3, 3, 0], [2, 4, 4, 0, 0, 0]]
+}]
 
 
-@pytest.mark.skipif(not torch.cuda.is_available(), reason='no CUDA')
-@pytest.mark.parametrize('tensor,i', product(tensors, range(len(data))))
-def test_forward_gpu(tensor, i):  # pragma: no cover
-    name = data[i]['name']
-    index = torch.cuda.LongTensor(data[i]['index'])
-    input = Tensor(tensor, data[i]['input']).cuda()
-    dim = data[i]['dim']
-    fill_value = data[i]['fill_value']
-    expected = torch.FloatTensor(data[i]['expected']).type_as(input)
-    output = expected.new(expected.size()).fill_(fill_value).cuda()
+@pytest.mark.parametrize('test,dtype,device', product(tests, dtypes, devices))
+def test_forward(test, dtype, device):
+    src = tensor(test['src'], dtype, device)
+    index = tensor(test['index'], torch.long, device)
 
-    func = getattr(torch_scatter, 'scatter_{}_'.format(name))
-    result = func(output, index, input, dim)
-    assert output.cpu().tolist() == expected.tolist()
-    if 'expected_arg' in data[i]:
-        expected_arg = torch.LongTensor(data[i]['expected_arg'])
-        assert result[1].cpu().tolist() == expected_arg.tolist()
-    func = getattr(torch_scatter, 'scatter_{}'.format(name))
-    result = func(index, input, dim, fill_value=fill_value)
-    if 'expected_arg' not in data[i]:
-        assert result.cpu().tolist() == expected.tolist()
-    else:
-        expected_arg = torch.LongTensor(data[i]['expected_arg'])
-        assert result[0].cpu().tolist() == expected.tolist()
-        assert result[1].cpu().tolist() == expected_arg.tolist()
+    op = getattr(torch_scatter, 'scatter_{}'.format(test['name']))
+    output = op(src, index, fill_value=test['fill_value'])
+
+    assert output.tolist() == test['expected']
+    # name = data[i]['name']
+    # index = torch.LongTensor(data[i]['index'])
+    # input = Tensor(tensor, data[i]['input'])
+    # dim = data[i]['dim']
+    # fill_value = data[i]['fill_value']
+    # expected = torch.FloatTensor(data[i]['expected']).type_as(input)
+    # output = expected.new(expected.size()).fill_(fill_value)
+
+    # func = getattr(torch_scatter, 'scatter_{}_'.format(name))
+    # result = func(output, index, input, dim)
+    # assert output.tolist() == expected.tolist()
+    # if 'expected_arg' in data[i]:
+    #     expected_arg = torch.LongTensor(data[i]['expected_arg'])
+    #     assert result[1].tolist() == expected_arg.tolist()
+
+    # func = getattr(torch_scatter, 'scatter_{}'.format(name))
+    # result = func(index, input, dim, fill_value=fill_value)
+    # if 'expected_arg' not in data[i]:
+    #     assert result.tolist() == expected.tolist()
+    # else:
+    #     expected_arg = torch.LongTensor(data[i]['expected_arg'])
+    #     assert result[0].tolist() == expected.tolist()
+    #     assert result[1].tolist() == expected_arg.tolist()
