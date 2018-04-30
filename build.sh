ece31b217283e8b08aefa574737f95b470d050d8
rusty1s <matthias.fey@tu-dortmund.de>
2018-04-30 08:11:53 +0200

    bugfix

--- build.sh
+++ build.sh
@@ -7,4 +7,4 @@ SRC_DIR=torch_scatter/kernel
 BUILD_DIR=torch_scatter/build
 
 mkdir -p $BUILD_DIR
-$(which nvcc) "-I$TORCH/lib/include" "-I$TORCH/lib/include/TH" "-I$TORCH/lib/include/THC" "-I$SRC_DIR" -c "$SRC_DIR/kernel.cu" -o "$BUILD_DIR/kernel.o" --compiler-options '-fPIC' -std=c++11
+$(which nvcc) "-I$TORCH/lib/include" "-I$TORCH/lib/include/TH" "-I$TORCH/lib/include/THC" "-I$SRC_DIR" -c "$SRC_DIR/kernel.cu" -o "$BUILD_DIR/kernel.so" --compiler-options '-fPIC' -std=c++11
