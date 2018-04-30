a29bc5a9de69f354ccae929fe264d312ecd27598
rusty1s <matthias.fey@tu-dortmund.de>
2018-04-30 07:54:37 +0200

    better compiler options

--- build.sh
+++ build.sh
@@ -7,4 +7,4 @@ SRC_DIR=torch_scatter/kernel
 BUILD_DIR=torch_scatter/build
 
 mkdir -p $BUILD_DIR
-$(which nvcc) -c -o "$BUILD_DIR/kernel.so" "$SRC_DIR/kernel.cu" -arch=sm_35 -Xcompiler -fPIC -shared "-I$TORCH/lib/include/TH" "-I$TORCH/lib/include/THC" "-I$SRC_DIR"
+$(which nvcc) "-I$TORCH/lib/include" "-I$TORCH/lib/include/TH" "-I$TORCH/lib/include/THC" "-I$SRC_DIR" -c "$SRC_DIR/kernel.cu" -o "$BUILD_DIR/kernel.o" --compiler-options '-fPIC' -std=c++11
