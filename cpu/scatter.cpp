7f68a505af66daba8678d1fa81b990de1aaf59ac
rusty1s <matthias.fey@tu-dortmund.de>
2018-08-06 17:15:09 +0200

    LAST TEST

--- cpu/scatter.cpp
+++ cpu/scatter.cpp
@@ -4,8 +4,9 @@
 
 void scatter_mul(at::Tensor src, at::Tensor index, at::Tensor out,
                  int64_t dim) {
-  int64_t elems_per_row = index.size(dim), i, idx;
-  printf("elems_per_row: %lli\n", elems_per_row);
+  printf("HUHUHUHU");
+  // int64_t elems_per_row = index.size(dim), i, idx;
+  // printf("elems_per_row: %lli\n", elems_per_row);
   // AT_DISPATCH_ALL_TYPES(src.type(), "scatter_mul", [&] {
   //   DIM_APPLY3(scalar_t, src, int64_t, index, scalar_t, out, dim, {
   //     for (i = 0; i < elems_per_row; i++) {
