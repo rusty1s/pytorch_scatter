3e409bf46d4c0775eed7df823b4509969193e82a
rusty1s <matthias.fey@tu-dortmund.de>
2018-11-28 14:40:02 +0100

    scatter_std

--- torch_scatter/__init__.py
+++ torch_scatter/__init__.py
@@ -3,12 +3,20 @@ from .sub import scatter_sub
 from .mul import scatter_mul
 from .div import scatter_div
 from .mean import scatter_mean
+from .std import scatter_std
 from .max import scatter_max
 from .min import scatter_min
 
 __version__ = '1.0.4'
 
 __all__ = [
-    'scatter_add', 'scatter_sub', 'scatter_mul', 'scatter_div', 'scatter_mean',
-    'scatter_max', 'scatter_min', '__version__'
+    'scatter_add',
+    'scatter_sub',
+    'scatter_mul',
+    'scatter_div',
+    'scatter_mean',
+    'scatter_std',
+    'scatter_max',
+    'scatter_min',
+    '__version__',
 ]
