5e69a3497fb4f607e4894b0047b28edcc1c1154d
rusty1s <matthias.fey@tu-dortmund.de>
2019-04-12 17:38:08 +0200

    only execute kernels if size(dim) > 0

--- torch_scatter/div.py
+++ torch_scatter/div.py
@@ -89,4 +89,6 @@ def scatter_div(src, index, dim=-1, out=None, dim_size=None, fill_value=1):
                [0.5000, 0.2500, 0.5000, 1.0000, 1.0000, 1.0000]])
     """
     src, out, index, dim = gen(src, index, dim, out, dim_size, fill_value)
+    if src.size(dim) == 0:  # pragma: no cover
+        return out
     return ScatterDiv.apply(out, src, index, dim)
