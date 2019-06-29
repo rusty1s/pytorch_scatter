64dea88458c90d8a35e2f0fe4a39c5b2a012b85c
rusty1s <matthias.fey@tu-dortmund.de>
2019-06-29 13:50:57 +0200

    fix py2.7

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ if CUDA_HOME is not None:
 __version__ = '1.3.0'
 url = 'https://github.com/rusty1s/pytorch_scatter'
 
-install_requires = []
+install_requires = ['future']
 setup_requires = ['pytest-runner']
 tests_require = ['pytest', 'pytest-cov']
 
