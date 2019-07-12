114c03f48059df0fed33a1d757a0f91dea4f2490
Johan Fleury <jfleury@arcaik.net>
2019-07-12 14:55:37 -0400

    fix: add torch as a build dependency
    This will allow adding `torch-scatter` and `torch` in the same
    `requirements.txt` file.

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ __version__ = '1.3.1'
 url = 'https://github.com/rusty1s/pytorch_scatter'
 
 install_requires = []
-setup_requires = ['pytest-runner']
+setup_requires = ['pytest-runner', 'torch']
 tests_require = ['pytest', 'pytest-cov']
 
 setup(
