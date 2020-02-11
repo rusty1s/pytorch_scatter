6d2704b2ca2a07fcd6ee4e7ea481de640d8f19ce
rusty1s <matthias.fey@tu-dortmund.de>
2020-02-11 16:31:29 +0100

    clean up

--- csrc/scatter.cpp
+++ csrc/scatter.cpp
@@ -1,34 +1,32 @@
 #include <Python.h>
 #include <torch/script.h>
 
-#include "cpu/scatter_cpu.h"
-#include "utils.h"
+// #include "cpu/scatter_cpu.h"
+// #include "utils.h"
 
-#ifdef WITH_CUDA
-#include "cuda/scatter_cuda.h"
-#endif
+// #ifdef WITH_CUDA
+// #include <cuda.h>
+// #include "cuda/scatter_cuda.h"
+// #endif
 
 #ifdef _WIN32
-#if PY_MAJOR_VERSION < 3
-PyMODINIT_FUNC init_C(void) { return NULL; }
-#else
 PyMODINIT_FUNC PyInit__C(void) { return NULL; }
-#endif
 #endif
 
 std::tuple<torch::Tensor, torch::optional<torch::Tensor>>
 scatter_fw(torch::Tensor src, torch::Tensor index, int64_t dim,
            torch::optional<torch::Tensor> optional_out,
            torch::optional<int64_t> dim_size, std::string reduce) {
-  if (src.device().is_cuda()) {
-#ifdef WITH_CUDA
-    return scatter_cuda(src, index, dim, optional_out, dim_size, reduce);
-#else
-    AT_ERROR("Not compiled with CUDA support");
-#endif
-  } else {
-    return scatter_cpu(src, index, dim, optional_out, dim_size, reduce);
-  }
+  return std::make_tuple(src, optional_out);
+  // if (src.device().is_cuda()) {
+  // #ifdef WITH_CUDA
+  //   return scatter_cuda(src, index, dim, optional_out, dim_size, reduce);
+  // #else
+  //   AT_ERROR("Not compiled with CUDA support");
+  // #endif
+  // } else {
+  //   return scatter_cpu(src, index, dim, optional_out, dim_size, reduce);
+  // }
 }
 
 static auto registry =
