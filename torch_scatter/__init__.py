fa27e1fddb2f9662207dc8a9d619611b057181a2
rusty1s <matthias.fey@tu-dortmund.de>
2020-02-11 19:03:14 +0100

    windows fix

--- torch_scatter/__init__.py
+++ torch_scatter/__init__.py
@@ -16,7 +16,20 @@ from .composite import (scatter_std, scatter_logsumexp, scatter_softmax,
 torch.ops.load_library(
     osp.join(osp.dirname(osp.abspath(__file__)), '_version.so'))
 cuda_version = torch.ops.torch_scatter.cuda_version()
-print(cuda_version)
+
+if cuda_version != 1 and torch.version.cuda is not None:
+    if cuda_version < 10000:
+        major, minor = int(str(cuda_version)[0]), int(str(cuda_version)[2])
+    else:
+        major, minor = int(str(cuda_version)[0:2]), int(str(cuda_version)[3])
+    t_major, t_minor = [int(x) for x in torch.version.cuda.split('.')]
+    if t_major != major or t_minor != minor:
+        raise RuntimeError(
+            'Detected that PyTorch and torch_scatter were compiled with '
+            'different CUDA versions. PyTorch has CUDA version={}.{} and '
+            'torch_scatter has CUDA version={}.{}. Please reinstall the '
+            'torch_scatter that matches your PyTorch install.'.format(
+                t_major, t_minor, major, minor))
 
 __version__ = '2.0.3'
 
