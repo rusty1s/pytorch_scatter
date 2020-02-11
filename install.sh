8bca192e6ca8d3dbdc0566dc2565755cb330ee10
rusty1s <matthias.fey@tu-dortmund.de>
2020-02-11 09:14:45 +0100

    update

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "$TRAVIS_OS_NAME" = "linux" ]; then
+if [ "$TRAVIS_OS_NAME" = "linux" ] && [ -z "${CUDA}" ]; then
   export INSTALLER=cuda-repo-${UBUNTU_VERSION}_${CUDA}_amd64.deb
   wget "http://developer.download.nvidia.com/compute/cuda/repos/${UBUNTU_VERSION}/x86_64/${INSTALLER}"
   sudo dpkg -i "${INSTALLER}"
