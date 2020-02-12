cb3493fd5003b63ff244702c4e14f76671b8da2d
rusty1s <matthias.fey@tu-dortmund.de>
2020-02-12 11:39:38 +0100

    add windows

--- script/conda.sh
+++ script/conda.sh
@@ -5,15 +5,16 @@ if [ "${TRAVIS_OS_NAME}" = "linux" ]; then
   chmod +x miniconda.sh
   ./miniconda.sh -b
   PATH=/home/travis/miniconda3/bin:${PATH}
-  conda update --yes conda
 fi
 
-if [ "${TRAVIS_OS_NAME}" = "macosx" ]; then
+if [ "${TRAVIS_OS_NAME}" = "osx" ]; then
   wget -q https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh
   chmod +x miniconda.sh
   ./miniconda.sh -b
   PATH=/home/travis/miniconda3/bin:${PATH}
-  conda update --yes conda
 fi
 
 
+if [ "${TRAVIS_OS_NAME}" = "windows" ]; then
+  choco install miniconda3
+fi
