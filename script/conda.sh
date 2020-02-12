ae88f6b10080278666140bfa74f109ad6e44671a
rusty1s <matthias.fey@tu-dortmund.de>
2020-02-12 10:37:29 +0100

    update

--- script/conda.sh
+++ script/conda.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 if [ "${TRAVIS_OS_NAME}" = "linux" ]; then
-  wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh
+  wget -q https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
   chmod +x miniconda.sh
   ./miniconda.sh -b
-  PATH=/home/travis/miniconda/bin:${PATH}
+  PATH=/home/travis/miniconda3/bin:${PATH}
   conda update --yes conda
 fi
 
