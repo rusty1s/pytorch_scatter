67e27e96fe527a92165d4df3b9baf5d4fa4c3772
rusty1s <matthias.fey@tu-dortmund.de>
2020-02-12 10:56:50 +0100

    typo

--- script/wheel.py
+++ script/wheel.py
@@ -6,11 +6,15 @@ objects = bucket.objects.all()
 wheels = sorted([obj.key[4:] for obj in objects if obj.key[-3:] == 'whl'])
 
 content = '<!DOCTYPE html>\n<html>\n<body>\n{}\n</body>\n</html>'
-url = 'https://pytorch-scatter.s3.eu-central-1.amazonaws.com/whl/{}.whl'
+url = 'https://pytorch-scatter.s3.eu-central-1.amazonaws.com/whl/{}'
 links = ['<a href="{}">{}</a><br/>'.format(url.format(w), w) for w in wheels]
 content = content.format('\n'.join(links))
 
 with open('index.html', 'w') as f:
     f.write(content)
 
-bucket.Object('whl/index.html').upload_file(Filename='index.html')
+bucket.Object('whl/index.html').upload_file(
+    Filename='index.html', ExtraArgs={
+        'ContentType': 'text/html',
+        'ACL': 'public-read'
+    })
