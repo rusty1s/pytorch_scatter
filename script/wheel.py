8047ccd2f85adafe9514d49a6d7a58badd47cb83
rusty1s <matthias.fey@tu-dortmund.de>
2020-02-12 10:44:50 +0100

    create wheel

--- script/wheel.py
+++ script/wheel.py
@@ -3,15 +3,14 @@ import boto3
 s3_resource = boto3.resource('s3')
 bucket = s3_resource.Bucket(name="pytorch-scatter")
 objects = bucket.objects.all()
-wheels = sorted([obj.key[4:] for obj in objects if obj.key[:3] == 'whl'])
-print(wheels)
+wheels = sorted([obj.key[4:] for obj in objects if obj.key[-3:] == 'whl'])
 
-# <!DOCTYPE html>
-# <html>
-#   <body>
-#     <a href="/frob/">frob</a>
-#     <a href="/spamspamspam/">spamspamspam</a>
-#   </body>
-# </html>
-
+content = '<!DOCTYPE html>\n<html>\n<body>\n{}\n</body>\n</html>'
 url = 'https://pytorch-scatter.s3.eu-central-1.amazonaws.com/whl/{}.whl'
+links = ['<a href="{}">{}</a><br/>'.format(url.format(w), w) for w in wheels]
+content = content.format('\n'.join(links))
+
+with open('index.html', 'w') as f:
+    f.write(content)
+
+bucket.Object('whl/index.html').upload_file(Filename='index.html')
