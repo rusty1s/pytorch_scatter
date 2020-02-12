6067286b34becb631f3cb096517e4bacac552258
rusty1s <matthias.fey@tu-dortmund.de>
2020-02-12 11:44:51 +0100

    fix os and windows

--- script/conda.sh
+++ script/conda.sh
@@ -11,10 +11,13 @@ if [ "${TRAVIS_OS_NAME}" = "osx" ]; then
   wget -q https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh
   chmod +x miniconda.sh
   ./miniconda.sh -b
-  PATH=/home/travis/miniconda3/bin:${PATH}
+  PATH=/Users/travis/miniconda3/bin:${PATH}
 fi
 
 
 if [ "${TRAVIS_OS_NAME}" = "windows" ]; then
   choco install miniconda3
+  PATH=/c/tools/miniconda3/Scripts:$PATH
 fi
+
+conda update --yes conda
