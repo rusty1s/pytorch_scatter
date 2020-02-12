56564cc583d720166a4c1a1b0e3c098beb79c266
rusty1s <matthias.fey@tu-dortmund.de>
2020-02-12 08:06:29 +0100

    fix

--- script/install.sh
+++ script/install.sh
@@ -20,7 +20,7 @@ if [ "${TRAVIS_OS_NAME}" = "linux" ] && [ "${IDX}" != "cpu" ]; then
 fi
 
 if [ "${TRAVIS_OS_NAME}" = "windows" ] && [ "${IDX}" != "cpu" ]; then
-  wget "https://developer.nvidia.com/compute/cuda/${CUDA_SHORT}/Prod2/local_installers2/cuda_${CUDA}_win10.exe"
+  wget "https://developer.nvidia.com/compute/cuda/${CUDA_SHORT}/Prod2/local_installers2/cuda_${CUDA}_win10"
   ls
   ./cuda_${CUDA}_win10.exe
 fi
