765003dd8f0c6ea6a6fee7a729db62f5d4bf8bd9
rusty1s <matthias.fey@tu-dortmund.de>
2020-02-12 16:04:30 +0100

    rename wheel

---
+++ script/rename_wheel.py
@@ -0,0 +1,19 @@
+import os
+import os.path as osp
+import glob
+
+dist_dir = osp.join(osp.dirname(osp.abspath(__file__)), '..', 'dist')
+wheels = glob.glob(osp.join('dist', '**', '*.whl'), recursive=True)
+
+for wheel in wheels:
+    idx = wheel.split(osp.sep)[-2]
+    if idx not in ['cpu', 'cu92', 'cu100', 'cu101']:
+        continue
+    name = wheel.split(osp.sep)[-1]
+    if idx in name:
+        continue
+
+    names = name.split('-')
+    name = '-'.join(names[:-4] + [names[-4] + '%2B' + idx] + names[-3:])
+    new_wheel = osp.join(*wheel.split(osp.sep)[:-1], name)
+    os.rename(wheel, new_wheel)
