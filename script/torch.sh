f007e92df960b283a1533b1a42a3ab4005ed6c82
rusty1s <matthias.fey@tu-dortmund.de>
2020-02-13 12:39:59 +0100

    potential mac fix

--- script/torch.sh
+++ script/torch.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-if [ "${TRAVIS_OS_NAME}" != "osx" ]; then
-  conda install pytorch="${TORCH_VERSION}" "${TOOLKIT}" -c pytorch --yes
-fi
-
-if [ "${TRAVIS_OS_NAME}" = "osx" ]; then
-  conda install pytorch="${TORCH_VERSION}" -c pytorch --yes
-fi
-
 # Fix "member may not be initialized" error on Windows: https://github.com/pytorch/pytorch/issues/27958
 if [ "${TRAVIS_OS_NAME}" = "windows" ] && [ "${IDX}" != "cpu" ]; then
   sed -i.bak -e 's/constexpr/const/g' /c/tools/miniconda3/envs/test/lib/site-packages/torch/include/torch/csrc/jit/script/module.h
