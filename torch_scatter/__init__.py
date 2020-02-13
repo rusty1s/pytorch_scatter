01036d5c6afde6b47a575b6d209ba58ae0c97afc
rusty1s <matthias.fey@tu-dortmund.de>
2020-02-13 14:08:36 +0100

    typo

--- torch_scatter/__init__.py
+++ torch_scatter/__init__.py
@@ -24,17 +24,6 @@ except OSError as e:
     else:
         raise OSError(e)
 
-from .scatter import (scatter_sum, scatter_add, scatter_mean, scatter_min,
-                      scatter_max, scatter)
-from .segment_csr import (segment_sum_csr, segment_add_csr, segment_mean_csr,
-                          segment_min_csr, segment_max_csr, segment_csr,
-                          gather_csr)
-from .segment_coo import (segment_sum_coo, segment_add_coo, segment_mean_coo,
-                          segment_min_coo, segment_max_coo, segment_coo,
-                          gather_coo)
-from .composite import (scatter_std, scatter_logsumexp, scatter_softmax,
-                        scatter_log_softmax)
-
 cuda_version = torch.ops.torch_scatter.cuda_version()
 if cuda_version != -1 and torch.version.cuda is not None:  # pragma: no cover
     if cuda_version < 10000:
@@ -51,6 +40,17 @@ if cuda_version != -1 and torch.version.cuda is not None:  # pragma: no cover
             f'{t_major}.{t_minor} and torch_scatter has CUDA version '
             f'{major}.{minor}. Please reinstall the torch_scatter that '
             f'matches your PyTorch install.')
+
+from .scatter import (scatter_sum, scatter_add, scatter_mean, scatter_min,
+                      scatter_max, scatter)
+from .segment_csr import (segment_sum_csr, segment_add_csr, segment_mean_csr,
+                          segment_min_csr, segment_max_csr, segment_csr,
+                          gather_csr)
+from .segment_coo import (segment_sum_coo, segment_add_coo, segment_mean_coo,
+                          segment_min_coo, segment_max_coo, segment_coo,
+                          gather_coo)
+from .composite import (scatter_std, scatter_logsumexp, scatter_softmax,
+                        scatter_log_softmax)
 
 __all__ = [
     'scatter_sum',
