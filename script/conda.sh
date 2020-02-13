fd265775a9e4169f909061b34df565f02c6c0326
rusty1s <matthias.fey@tu-dortmund.de>
2020-02-13 12:33:55 +0100

    fix install

--- script/conda.sh
+++ script/conda.sh
@@ -24,12 +24,3 @@ fi
 conda update --yes conda
 
 conda create --yes -n test python="${PYTHON_VERSION}"
-source activate test
-conda install pytorch="${TORCH_VERSION}" "${TOOLKIT}" -c pytorch --yes
-
-# Fix "member may not be initialized" error on Windows: https://github.com/pytorch/pytorch/issues/27958
-if [ "${TRAVIS_OS_NAME}" = "windows" ] && [ "${IDX}" != "cpu" ]; then
-  sed -i.bak -e 's/constexpr/const/g' /c/tools/miniconda3/envs/test/lib/site-packages/torch/include/torch/csrc/jit/script/module.h
-  sed -i.bak -e 's/constexpr/const/g' /c/tools/miniconda3/envs/test/lib/site-packages/torch/include/torch/csrc/jit/argument_spec.h
-  sed -i.bak -e 's/return \*(this->value)/return \*((type\*)this->value)/g' /c/tools/miniconda3/envs/test/lib/site-packages/torch/include/pybind11/cast.h
-fi
---
+++ script/torch.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+conda install pytorch="${TORCH_VERSION}" "${TOOLKIT}" -c pytorch --yes
+
+# Fix "member may not be initialized" error on Windows: https://github.com/pytorch/pytorch/issues/27958
+if [ "${TRAVIS_OS_NAME}" = "windows" ] && [ "${IDX}" != "cpu" ]; then
+  sed -i.bak -e 's/constexpr/const/g' /c/tools/miniconda3/envs/test/lib/site-packages/torch/include/torch/csrc/jit/script/module.h
+  sed -i.bak -e 's/constexpr/const/g' /c/tools/miniconda3/envs/test/lib/site-packages/torch/include/torch/csrc/jit/argument_spec.h
+  sed -i.bak -e 's/return \*(this->value)/return \*((type\*)this->value)/g' /c/tools/miniconda3/envs/test/lib/site-packages/torch/include/pybind11/cast.h
+fi
+
