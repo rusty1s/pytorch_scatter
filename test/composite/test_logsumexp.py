f20d874278e598abc15c6bc34eff689d64254809
rusty1s <matthias.fey@tu-dortmund.de>
2020-04-06 12:32:45 +0200

    logsumexp cleanup

--- test/composite/test_logsumexp.py
+++ test/composite/test_logsumexp.py
@@ -3,19 +3,18 @@ from torch_scatter import scatter_logsumexp
 
 
 def test_logsumexp():
-    src = torch.tensor([0.5, 0, 0.5, -2.1, 3.2, 7, -1, -100])
-    src.requires_grad_()
-    index = torch.tensor([0, 1, 0, 1, 1, 2, 4, 4])
+    inputs = torch.tensor([
+        0.5, 0.5, 0.0, -2.1, 3.2, 7.0, -1.0, -100.0,
+        float('-inf'),
+        float('-inf'), 0.0
+    ])
+    inputs.requires_grad_()
+    index = torch.tensor([0, 0, 1, 1, 1, 2, 4, 4, 5, 6, 6])
+    splits = [2, 3, 1, 0, 2, 1, 2]
 
-    out = scatter_logsumexp(src, index)
+    outputs = scatter_logsumexp(inputs, index)
 
-    out0 = torch.logsumexp(torch.tensor([0.5, 0.5]), dim=-1)
-    out1 = torch.logsumexp(torch.tensor([0, -2.1, 3.2]), dim=-1)
-    out2 = torch.logsumexp(torch.tensor(7, dtype=torch.float), dim=-1)
-    out3 = torch.logsumexp(torch.tensor([], dtype=torch.float), dim=-1)
-    out4 = torch.tensor(-1, dtype=torch.float)
+    for src, out in zip(inputs.split(splits), outputs.unbind()):
+        assert out.tolist() == torch.logsumexp(src, dim=0).tolist()
 
-    expected = torch.stack([out0, out1, out2, out3, out4], dim=0)
-    assert torch.allclose(out, expected)
-
-    out.backward(torch.randn_like(out))
+    outputs.backward(torch.randn_like(outputs))
