5379ec20a805c6694da147e25282b0976af44ea9
rusty1s <matthias.fey@tu-dortmund.de>
2020-04-23 13:48:04 +0200

    fix

--- script/torch.sh
+++ script/torch.sh
@@ -2,8 +2,8 @@
 
 # Fix "member may not be initialized" error on Windows: https://github.com/pytorch/pytorch/issues/27958
 if [ "${TRAVIS_OS_NAME}" = "windows" ] && [ "${IDX}" != "cpu" ]; then
-  sed -i.bak -e 's/constexpr/const/g' /c/tools/miniconda3/envs/test/lib/site-packages/torch/include/torch/csrc/api/script/module.h
-  sed -i.bak -e 's/constexpr/const/g' /c/tools/miniconda3/envs/test/lib/site-packages/torch/include/torch/csrc/runtime/argument_spec.h
+  sed -i.bak -e 's/constexpr/const/g' /c/tools/miniconda3/envs/test/lib/site-packages/torch/include/torch/csrc/jit/api/script/module.h
+  sed -i.bak -e 's/constexpr/const/g' /c/tools/miniconda3/envs/test/lib/site-packages/torch/include/torch/csrc/jit/runtime/argument_spec.h
   sed -i.bak -e 's/return \*(this->value)/return \*((type\*)this->value)/g' /c/tools/miniconda3/envs/test/lib/site-packages/torch/include/pybind11/cast.h
 fi
 
