f2510bfbd1adadffce907ec42c159841a064032c
rusty1s <matthias.fey@tu-dortmund.de>
2020-04-23 10:45:32 +0200

    update gcc

---
+++ script/gcc.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ "${TRAVIS_OS_NAME}" = "linux" ]; then
+  sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+  sudo apt install gcc-7 g++-7 --yes
+  export CC=gcc-7
+  export CXX=g++-7
+fi
+
