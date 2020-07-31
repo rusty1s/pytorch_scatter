b9d33690f4814513e17942052cfa3dd0f6b058f3
rusty1s <matthias.fey@tu-dortmund.de>
2020-07-31 15:52:41 +0200

    fix

--- script/torch.sh
+++ script/torch.sh
@@ -11,5 +11,5 @@ fi
 # https://github.com/pytorch/pytorch/commit/d2e16dd888a9b5fd55bd475d4fcffb70f388d4f0
 if [ "${TRAVIS_OS_NAME}" = "windows" ] && [ "${TORCH_VERSION}" = "1.6.0" ]; then
   echo "Fix nvcc for PyTorch 1.6.0"
-  sed -i.bak -e 's/constexpr/CONSTEXPR_EXCEPT_WIN_CUDA/g' /c/tools/miniconda3/envs/test/lib/site-packages/torch/include/torch/csrc/jit/api/module.h
+  sed -i.bak -e 's/CONSTEXPR_EXCEPT_WIN_CUDA/const/g' /c/tools/miniconda3/envs/test/lib/site-packages/torch/include/torch/csrc/jit/api/module.h
 fi
