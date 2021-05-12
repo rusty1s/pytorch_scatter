5dce51cef6350b82cf034adb79b4d8f80feb461b
rusty1s <matthias.fey@tu-dortmund.de>
2021-05-12 11:15:55 +0200

    remove absolute paths in SOURCES.txt

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ BUILD_DOCS = os.getenv('BUILD_DOCS', '0') == '1'
 def get_extensions():
     extensions = []
 
-    extensions_dir = osp.join(osp.dirname(osp.abspath(__file__)), 'csrc')
+    extensions_dir = osp.join('csrc')
     main_files = glob.glob(osp.join(extensions_dir, '*.cpp'))
 
     for main, suffix in product(main_files, suffices):
