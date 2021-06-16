070850cf8a5b4591e6d699363f88bd970973d63d
rusty1s <matthias.fey@tu-dortmund.de>
2021-06-16 12:20:49 +0200

    build everything

---
+++ .github/workflows/cuda/cu101-Windows-env.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+CUDA_HOME=/c/Program\ Files/NVIDIA\ GPU\ Computing\ Toolkit/CUDA/v10.1
+PATH=${CUDA_HOME}/bin:$PATH
+PATH=/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2017/BuildTools/MSBuild/15.0/Bin:$PATH
