40e82097db4449f91c97b932de61469825ca7f6c
rusty1s <matthias.fey@tu-dortmund.de>
2021-06-16 14:36:30 +0200

    update

--- .github/workflows/cuda/cu102-Windows-env.sh
+++ .github/workflows/cuda/cu102-Windows-env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export CUDA_HOME=/c/Program\ Files/NVIDIA\ GPU\ Computing\ Toolkit/CUDA/v10.2
-export PATH=${CUDA_HOME}/bin:$PATH
-export PATH=/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2017/BuildTools/MSBuild/15.0/Bin:$PATH
+CUDA_HOME=/c/Program\ Files/NVIDIA\ GPU\ Computing\ Toolkit/CUDA/v10.2
+PATH=${CUDA_HOME}/bin:$PATH
+PATH=/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2017/BuildTools/MSBuild/15.0/Bin:$PATH
--- .github/workflows/cuda/cu111-Linux-env.sh
+++ .github/workflows/cuda/cu111-Linux-env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export CUDA_HOME=/usr/local/cuda-11.1
-export LD_LIBRARY_PATH=${CUDA_HOME}/lib64:${LD_LIBRARY_PATH}
-export PATH=${CUDA_HOME}/bin:${PATH}
+CUDA_HOME=/usr/local/cuda-11.1
+LD_LIBRARY_PATH=${CUDA_HOME}/lib64:${LD_LIBRARY_PATH}
+PATH=${CUDA_HOME}/bin:${PATH}
