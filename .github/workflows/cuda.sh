568c0830250fbade22926581e376bcf8ef213c3f
rusty1s <matthias.fey@tu-dortmund.de>
2021-06-16 09:35:19 +0200

    [skip ci] list cuda version

--- .github/workflows/cuda.sh
+++ .github/workflows/cuda.sh
@@ -19,6 +19,7 @@ sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
 sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/7fa2af80.pub
 sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
 sudo apt-get update -qq
+sudo apt-cache search cuda-core
 sudo apt-get install "cuda-core-11-1" "cuda-nvcc-11-1" "cuda-libraries-dev-11-1"
 
 nvcc --version
