199377357503a5d0a48f6e8e2428800e9053fcb4
rusty1s <matthias.fey@tu-dortmund.de>
2021-06-16 10:45:41 +0200

    [skip ci] update cuda installation

--- .github/workflows/cuda/cu111-Linux.sh
+++ .github/workflows/cuda/cu111-Linux.sh
@@ -11,9 +11,3 @@ sudo apt install cuda-nvcc-11-1 cuda-libraries-dev-11-1
 sudo apt clean
 
 rm -f https://developer.download.nvidia.com/compute/cuda/11.1.1/local_installers/cuda-repo-ubuntu2004-11-1-local_11.1.1-455.32.00-1_amd64.deb
-
-CUDA_HOME=/usr/local/cuda-11.1
-LD_LIBRARY_PATH=${CUDA_HOME}/lib64:${LD_LIBRARY_PATH}
-PATH=${CUDA_HOME}/bin:${PATH}
-
-nvcc --version
