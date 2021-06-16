8c66f0cd29af549b3fc6ed440f1bb792fa8615e4
rusty1s <matthias.fey@tu-dortmund.de>
2021-06-16 19:24:33 +0200

    [skip ci] update to cuda 10.1.2

--- .github/workflows/cuda/cu101-Windows.sh
+++ .github/workflows/cuda/cu101-Windows.sh
@@ -6,7 +6,7 @@ curl -k -L "https://drive.google.com/u/0/uc?id=1injUyo3lnarMgWyRcXqKg4UGnN0ysmuq
 7z x "/tmp/gpu_driver_dlls.zip" -o"/c/Windows/System32"
 
 export CUDA_SHORT=10.1
-export CUDA_URL=https://developer.nvidia.com/compute/cuda/${CUDA_SHORT}/Prod/local_installers
+export CUDA_URL=https://developer.download.nvidia.com/compute/cuda/${CUDA_SHORT}/Prod/local_installers/
 export CUDA_FILE=cuda_${CUDA_SHORT}.243_426.00_win10.exe
 
 # Install CUDA:
