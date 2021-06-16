db22d9ffb4badc07ef10e0bffdee653299e2bc24
rusty1s <matthias.fey@tu-dortmund.de>
2021-06-16 16:18:54 +0200

    [skip ci] bash shell

--- .github/workflows/cuda/cu102-Windows.sh
+++ .github/workflows/cuda/cu102-Windows.sh
@@ -11,11 +11,6 @@ export CUDA_FILE=cuda_${CUDA_SHORT}.89_441.22_win10.exe
 
 # Install CUDA:
 curl -k -L "${CUDA_URL}/${CUDA_FILE}" --output "${CUDA_FILE}"
-ls -lah
-echo ""
-echo "Downloading ${CUDA_URL}/${CUDA_FILE}..."
-PowerShell -Command "Invoke-WebRequest -OutFile \"${CUDA_FILE}\" -Uri \"${CUDA_URL}/${CUDA_FILE}\""
-echo "Done!"
 echo ""
 echo "Installing from ${CUDA_FILE}..."
 PowerShell -Command "Start-Process -FilePath \"${CUDA_FILE}\" -ArgumentList \"-s nvcc_${CUDA_SHORT} cuobjdump_${CUDA_SHORT} nvprune_${CUDA_SHORT} cupti_${CUDA_SHORT} cublas_dev_${CUDA_SHORT} cudart_${CUDA_SHORT} cufft_dev_${CUDA_SHORT} curand_dev_${CUDA_SHORT} cusolver_dev_${CUDA_SHORT} cusparse_dev_${CUDA_SHORT} npp_dev_${CUDA_SHORT} nvrtc_dev_${CUDA_SHORT} nvml_dev_${CUDA_SHORT}\" -Wait -NoNewWindow"
