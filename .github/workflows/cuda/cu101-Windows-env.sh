f10b28be876fea9e4abeb5e11bbc05b613fbc606
rusty1s <matthias.fey@tu-dortmund.de>
2021-09-07 12:51:50 +0200

    [ci skip]

--- .github/workflows/cuda/cu101-Windows-env.sh
+++ .github/workflows/cuda/cu101-Windows-env.sh
@@ -3,3 +3,6 @@
 CUDA_HOME=/c/Program\ Files/NVIDIA\ GPU\ Computing\ Toolkit/CUDA/v10.1
 PATH=${CUDA_HOME}/bin:$PATH
 PATH=/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2017/BuildTools/MSBuild/15.0/Bin:$PATH
+
+export FORCE_CUDA=1
+export TORCH_CUDA_ARCH_LIST="3.5;5.0+PTX;6.0;7.0;7.5"
