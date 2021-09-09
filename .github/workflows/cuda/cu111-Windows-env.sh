bbdf3c48d302b20fc04450b2992e06fbac3299ec
rusty1s <matthias.fey@tu-dortmund.de>
2021-09-09 09:34:49 +0200

    [ci skip]

--- .github/workflows/cuda/cu111-Windows-env.sh
+++ .github/workflows/cuda/cu111-Windows-env.sh
@@ -5,4 +5,4 @@ PATH=${CUDA_HOME}/bin:$PATH
 PATH=/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2017/BuildTools/MSBuild/15.0/Bin:$PATH
 
 export FORCE_CUDA=1
-export TORCH_CUDA_ARCH_LIST="6.0+PTX"
+export TORCH_CUDA_ARCH_LIST="3.5;5.0+PTX;6.0;7.0;7.5"
