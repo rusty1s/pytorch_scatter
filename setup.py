8c58ddb4d145c590d091d7d9914127333a21f658
rusty1s <matthias.fey@tu-dortmund.de>
2021-09-09 10:47:22 +0200

    [ci skip]

--- setup.py
+++ setup.py
@@ -50,6 +50,8 @@ def get_extensions():
             nvcc_flags = [] if nvcc_flags == '' else nvcc_flags.split(' ')
             nvcc_flags += ['--expt-relaxed-constexpr', '-O2']
             extra_compile_args['nvcc'] = nvcc_flags
+            if sys.platform == 'win32':
+                extra_compile_args['nvcc'].append('/Zm1000')
 
         name = main.split(os.sep)[-1][:-4]
         sources = [main]
