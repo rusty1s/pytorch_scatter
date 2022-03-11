22b7aed69935f13d78c02a0bfe65a3ca1ea32f93
rusty1s <matthias.fey@tu-dortmund.de>
2022-03-11 09:57:31 +0000

    [ci skip]

--- .github/workflows/cuda/cu115-Windows-env.sh
+++ .github/workflows/cuda/cu115-Windows-env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CUDA_HOME=/c/Program\ Files/NVIDIA\ GPU\ Computing\ Toolkit/CUDA/v11.5
+CUDA_HOME=/c/Program\ Files/NVIDIA\ GPU\ Computing\ Toolkit/CUDA/v11.3
 PATH=${CUDA_HOME}/bin:$PATH
 PATH=/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2017/BuildTools/MSBuild/15.0/Bin:$PATH
 
--- .github/workflows/cuda/cu115-Windows.sh
+++ .github/workflows/cuda/cu115-Windows.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-export CUDA_SHORT=11.5
-export CUDA_URL=https://developer.download.nvidia.com/compute/cuda/${CUDA_SHORT}.2/local_installers
-export CUDA_FILE=cuda_${CUDA_SHORT}.2_496.13_windows.exe
+# TODO We currently use CUDA 11.3 to build CUDA 11.5 Windows wheels
+
+# Install NVIDIA drivers, see:
+# https://github.com/pytorch/vision/blob/master/packaging/windows/internal/cuda_install.bat#L99-L102
+curl -k -L "https://drive.google.com/u/0/uc?id=1injUyo3lnarMgWyRcXqKg4UGnN0ysmuq&export=download" --output "/tmp/gpu_driver_dlls.zip"
+7z x "/tmp/gpu_driver_dlls.zip" -o"/c/Windows/System32"
+
+export CUDA_SHORT=11.3
+export CUDA_URL=https://developer.download.nvidia.com/compute/cuda/${CUDA_SHORT}.0/local_installers
+export CUDA_FILE=cuda_${CUDA_SHORT}.0_465.89_win10.exe
 
 # Install CUDA:
 curl -k -L "${CUDA_URL}/${CUDA_FILE}" --output "${CUDA_FILE}"
