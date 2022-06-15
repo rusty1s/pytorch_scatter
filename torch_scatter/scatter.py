146b304b921581eacc8713470c3e6f90a0d4af83
Mikayla Gawarecki <mikaylagawarecki@gmail.com>
2022-06-15 21:38:50 +0000

    remove commented code in scatter_mean

--- torch_scatter/scatter.py
+++ torch_scatter/scatter.py
@@ -32,24 +32,6 @@ def scatter_mul(src: torch.Tensor, index: torch.Tensor, dim: int = -1,
 def scatter_mean(src: torch.Tensor, index: torch.Tensor, dim: int = -1,
                  out: Optional[torch.Tensor] = None,
                  dim_size: Optional[int] = None) -> torch.Tensor:
-    # out = scatter_sum(src, index, dim, out, dim_size)
-    # dim_size = out.size(dim)
-
-    # index_dim = dim
-    # if index_dim < 0:
-    #     index_dim = index_dim + src.dim()
-    # if index.dim() <= index_dim:
-    #     index_dim = index.dim() - 1
-
-    # ones = torch.ones(index.size(), dtype=src.dtype, device=src.device)
-    # count = scatter_sum(ones, index, index_dim, None, dim_size)
-    # count[count < 1] = 1
-    # count = broadcast(count, out, dim)
-    # if out.is_floating_point():
-    #     out.true_divide_(count)
-    # else:
-    #     out.div_(count, rounding_mode='floor')
-    # return out
     # FIXME: when index.dim() == 1, use index_reduce and don't do the broadcast
     index = broadcast(index, src, dim)
     include = True
