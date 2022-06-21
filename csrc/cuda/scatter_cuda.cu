ecfb66f40247a8161871e2dae2c73a7055e96fb4
Mikayla Gawarecki <mikaylagawarecki@gmail.com>
2022-06-21 21:07:10 +0000

    Resolve overflow issues in scatter_cuda kernel to allow benchmarking

--- csrc/cuda/scatter_cuda.cu
+++ csrc/cuda/scatter_cuda.cu
@@ -7,24 +7,37 @@
 #include "reducer.cuh"
 #include "utils.cuh"
 
+#include <stdio.h>
+#include <limits>
+
 #define THREADS 1024
 #define BLOCKS(N) (N + THREADS - 1) / THREADS
 
 template <typename scalar_t, ReductionType REDUCE>
 __global__ void
 scatter_kernel(const scalar_t *src_data,
-               const at::cuda::detail::TensorInfo<int64_t, int> index_info,
-               scalar_t *out_data, int E, int K, int N, int numel) {
+               // similarly make IndexType int64 to allow indexing with int64s
+               const at::cuda::detail::TensorInfo<int64_t, int64_t> index_info,
+               scalar_t *out_data,
+               // make all the constants int64
+               int64_t E, int64_t K, int64_t N, int64_t numel) {
 
-  int thread_idx = blockIdx.x * blockDim.x + threadIdx.x;
+  // changing thread_idx from int32 to int64 so that this logic can handle
+  // numel larger than 2^31 - 1
 
-  int b = thread_idx / (E * K);
-  int k = thread_idx % K;
+  // FIXME: why is the cast on the RHS necessary???
+  // without it the max thread_idx I was getting was 429467296 iirc which is
+  // exactly what fits 32 bits, are blockDim.x etc all ints?
+  // but online I've never seen code that casts on the RHS
+  const int64_t thread_idx = (int64_t) blockIdx.x * (int64_t) blockDim.x + (int64_t) threadIdx.x;
+
+  auto b = thread_idx / (E * K);
+  auto k = thread_idx % K;
 
   if (thread_idx < numel) {
-    int offset = at::cuda::detail::IndexToOffset<int64_t, int, -1>::get(
+    int64_t offset = at::cuda::detail::IndexToOffset<int64_t, int64_t, -1>::get(
         thread_idx, index_info);
-    int64_t idx = index_info.data[offset];
+    int64_t idx = index_info.data[offset]; 
 
     Reducer<scalar_t, REDUCE>::atomic_write(out_data + b * N * K + idx * K + k,
                                             src_data[thread_idx]);
@@ -34,18 +47,18 @@ scatter_kernel(const scalar_t *src_data,
 template <typename scalar_t>
 __global__ void
 scatter_arg_kernel(const scalar_t *src_data,
-                   const at::cuda::detail::TensorInfo<int64_t, int> index_info,
-                   const scalar_t *out_data, int64_t *arg_out_data, int E,
-                   int K, int N, int numel) {
+                   const at::cuda::detail::TensorInfo<int64_t, int64_t> index_info,
+                   const scalar_t *out_data, int64_t *arg_out_data, int64_t E,
+                   int64_t K, int64_t N, int64_t numel) {
 
-  int thread_idx = blockIdx.x * blockDim.x + threadIdx.x;
+  const int64_t thread_idx = (int64_t) (blockIdx.x * blockDim.x + threadIdx.x);
 
-  int b = thread_idx / (E * K);
-  int e = (thread_idx / K) % E;
-  int k = thread_idx % K;
+  auto b = thread_idx / (E * K);
+  auto e = (thread_idx / K) % E;
+  auto k = thread_idx % K;
 
   if (thread_idx < numel) {
-    int offset = at::cuda::detail::IndexToOffset<int64_t, int, -1>::get(
+    int64_t offset = at::cuda::detail::IndexToOffset<int64_t, int64_t, -1>::get(
         thread_idx, index_info);
     int64_t idx = index_info.data[offset];
 
@@ -109,7 +122,8 @@ scatter_cuda(torch::Tensor src, torch::Tensor index, int64_t dim,
   auto K = src.numel() / (B * E);
   auto N = out.size(dim);
 
-  auto index_info = at::cuda::detail::getTensorInfo<int64_t, int>(index);
+  // make IndexType int64 to allow indexing with int64s
+  auto index_info = at::cuda::detail::getTensorInfo<int64_t, int64_t>(index);
   auto stream = at::cuda::getCurrentCUDAStream();
   AT_DISPATCH_ALL_TYPES_AND(at::ScalarType::Half, src.scalar_type(), "_", [&] {
     auto src_data = src.data_ptr<scalar_t>();
@@ -126,11 +140,12 @@ scatter_cuda(torch::Tensor src, torch::Tensor index, int64_t dim,
       if (!optional_out.has_value() && (REDUCE == MIN || REDUCE == MAX))
         out.masked_fill_(out == Reducer<scalar_t, REDUCE>::init(), (scalar_t)0);
 
-      if (REDUCE == MIN || REDUCE == MAX)
-        scatter_arg_kernel<scalar_t>
-            <<<BLOCKS(src.numel()), THREADS, 0, stream>>>(
-                src_data, index_info, out_data, arg_out_data, E, K, N,
-                src.numel());
+      // Comment arg kernel out so comparison for benchmarks is fair
+      // if (REDUCE == MIN || REDUCE == MAX)
+      //   scatter_arg_kernel<scalar_t>
+      //       <<<BLOCKS(src.numel()), THREADS, 0, stream>>>(
+      //           src_data, index_info, out_data, arg_out_data, E, K, N,
+      //           src.numel());
     });
   });
 
