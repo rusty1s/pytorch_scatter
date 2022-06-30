06321b590a440c538d471527500a0e109b172aba
rusty1s <matthias.fey@tu-dortmund.de>
2022-06-30 14:51:39 +0000

    fix conda build [ci skip]

--- conda/pytorch-scatter/build_conda.sh
+++ conda/pytorch-scatter/build_conda.sh
@@ -39,4 +39,8 @@ echo "PyTorch $TORCH_VERSION+$CUDA_VERSION"
 echo "- $CONDA_PYTORCH_CONSTRAINT"
 echo "- $CONDA_CUDATOOLKIT_CONSTRAINT"
 
-conda build . -c pytorch -c default -c nvidia -c conda-forge --output-folder "$HOME/conda-bld"
+if [ "${CUDA_VERSION}" = "cu116" ]; then
+  conda build . -c pytorch -c default -c nvidia -c conda-forge --output-folder "$HOME/conda-bld"
+else
+  conda build . -c pytorch -c default -c nvidia --output-folder "$HOME/conda-bld"
+fi
