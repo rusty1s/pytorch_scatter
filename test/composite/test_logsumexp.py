dbf42c4017446c18b46b810078aaabb6355cf675
rusty1s <matthias.fey@tu-dortmund.de>
2023-04-14 08:52:38 +0000

    fix test

--- test/composite/test_logsumexp.py
+++ test/composite/test_logsumexp.py
@@ -4,18 +4,26 @@ from torch_scatter import scatter_logsumexp
 
 def test_logsumexp():
     inputs = torch.tensor([
-        0.5, 0.5, 0.0, -2.1, 3.2, 7.0, -1.0, -100.0,
-        float('-inf'),
-        float('-inf'), 0.0
+        0.5,
+        0.5,
+        0.0,
+        -2.1,
+        3.2,
+        7.0,
+        -1.0,
+        -100.0,
     ])
     inputs.requires_grad_()
-    index = torch.tensor([0, 0, 1, 1, 1, 2, 4, 4, 5, 6, 6])
-    splits = [2, 3, 1, 0, 2, 1, 2]
+    index = torch.tensor([0, 0, 1, 1, 1, 2, 4, 4])
+    splits = [2, 3, 1, 0, 2]
 
     outputs = scatter_logsumexp(inputs, index)
 
     for src, out in zip(inputs.split(splits), outputs.unbind()):
-        assert out.tolist() == torch.logsumexp(src, dim=0).tolist()
+        if src.numel() > 0:
+            assert out.tolist() == torch.logsumexp(src, dim=0).tolist()
+        else:
+            assert out.item() == 0.0
 
     outputs.backward(torch.randn_like(outputs))
 
