140d3ad677aae615767412873b90982cbf97d35d
Matthias Fey <matthias.fey@tu-dortmund.de>
2023-10-05 20:36:18 +0200

    update (#395)

--- .github/workflows/cuda/cu117-Linux.sh
+++ .github/workflows/cuda/cu117-Linux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-OS=ubuntu1804
+OS=ubuntu2004
 
 wget -nv https://developer.download.nvidia.com/compute/cuda/repos/${OS}/x86_64/cuda-${OS}.pin
 sudo mv cuda-${OS}.pin /etc/apt/preferences.d/cuda-repository-pin-600
