5a78ff480cd4f9b902df27093c69815b083f8e5b
rusty1s <matthias.fey@tu-dortmund.de>
2024-08-15 06:12:01 +0000

    update

---
+++ .github/workflows/cuda/cu124-Linux.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+OS=ubuntu2004
+
+wget -nv https://developer.download.nvidia.com/compute/cuda/repos/${OS}/x86_64/cuda-${OS}.pin
+sudo mv cuda-${OS}.pin /etc/apt/preferences.d/cuda-repository-pin-600
+wget -nv https://developer.download.nvidia.com/compute/cuda/12.4.1/local_installers/cuda-repo-${OS}-12-4-local_12.4.1-550.54.15-1_amd64.deb
+sudo dpkg -i cuda-repo-${OS}-12-4-local_12.4.1-550.54.15-1_amd64.deb
+sudo cp /var/cuda-repo-${OS}-12-4-local/cuda-*-keyring.gpg /usr/share/keyrings/
+
+sudo apt-get -qq update
+sudo apt install cuda-nvcc-12-4 cuda-libraries-dev-12-4
+sudo apt clean
+
+rm -f https://developer.download.nvidia.com/compute/cuda/12.4.0/local_installers/cuda-repo-${OS}-12-4-local_12.4.1-550.54.15-1_amd64.deb
--- conda/pytorch-scatter/README.md
+++ conda/pytorch-scatter/README.md
@@ -1,3 +1,3 @@
 ```
-./build_conda.sh 3.11 2.3.0 cu118  # python, pytorch and cuda version
+./build_conda.sh 3.11 2.4.0 cu118  # python, pytorch and cuda version
 ```
