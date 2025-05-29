e2864618fa14a3b44bfd7e60d7241e903301c5a7
rusty1s <matthias.fey@tu-dortmund.de>
2025-05-29 22:02:33 +0200

    update

--- .github/workflows/cuda/cu124-Linux.sh
+++ .github/workflows/cuda/cu124-Linux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-OS=ubuntu2004
+OS=ubuntu2204
 
 wget -nv https://developer.download.nvidia.com/compute/cuda/repos/${OS}/x86_64/cuda-${OS}.pin
 sudo mv cuda-${OS}.pin /etc/apt/preferences.d/cuda-repository-pin-600
