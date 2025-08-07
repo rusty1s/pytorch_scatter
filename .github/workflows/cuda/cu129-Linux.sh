bf935a70f51b2bf30f1c0f4a87012262b87a6a17
Akihiro Nitta <nitta@akihironitta.com>
2025-08-07 20:22:42 +0000

    update

---
+++ .github/workflows/cuda/cu129-Linux.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+OS=ubuntu2204
+
+wget -nv https://developer.download.nvidia.com/compute/cuda/repos/${OS}/x86_64/cuda-${OS}.pin
+sudo mv cuda-${OS}.pin /etc/apt/preferences.d/cuda-repository-pin-600
+
+wget -nv https://developer.download.nvidia.com/compute/cuda/12.9.1/local_installers/cuda-repo-${OS}-12-9-local_12.9.1-575.57.08-1_amd64.deb
+
+sudo dpkg -i cuda-repo-${OS}-12-9-local_12.9.1-575.57.08-1_amd64.deb
+sudo cp /var/cuda-repo-${OS}-12-9-local/cuda-*-keyring.gpg /usr/share/keyrings/
+
+sudo apt-get -qq update
+sudo apt install cuda-nvcc-12-9 cuda-libraries-dev-12-9
+sudo apt clean
+
+rm -f *.deb
